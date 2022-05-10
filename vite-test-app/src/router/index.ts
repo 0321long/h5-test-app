a0595cefabd8478b17e10c0e0a43b128bf7d14a8
long <321041080@qq.com>
2022-05-10 10:30:57 +0800

    update_001

--- vite-test-app/src/router/index.ts
+++ vite-test-app/src/router/index.ts
@@ -4,11 +4,11 @@
  * @Author: 
  * @Date: 2022-05-10 09:17:06
  * @LastEditors: long
- * @LastEditTime: 2022-05-10 10:13:35
+ * @LastEditTime: 2022-05-10 10:30:04
  */
 import { createRouter, createWebHashHistory } from 'vue-router';
 
-const Home = () => import('../views/Home/index.vue')
+const Home = () => import('@/views/Home/index.vue')
 const routes = [
   {
     path: '/',
